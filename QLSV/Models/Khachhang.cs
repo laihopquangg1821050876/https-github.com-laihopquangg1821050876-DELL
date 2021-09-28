668f012f689abe4e66369af0f5cbf56106a44743
quanga123 <quangoc0302@gmail.com>
2021-09-28 10:24:21 +0700

    quang

---
+++ QLSV/Models/Khachhang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QLSV.Models
+{
+    [Key]
+    public class Khachhang
+    {
+        public string makh { get; set; }
+        public string tenkh {get; set; }
+        public DateTime sinhngay { get; set; }
+        public string điachi { get; set; }
+        public int sodienthoai { get; }
+    }
+}
